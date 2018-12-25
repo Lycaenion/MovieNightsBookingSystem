15f154db1f3bee30981f57c39d0f273e6e57bacc
Linn Mårtensson <linn.martensson@hotmail.se>
2018-12-25 17:04:24 +0100

    connection with db

--- MovieNightsBookingSystem/Backend/src/main/java/program/controllers/MovieController.java
+++ MovieNightsBookingSystem/Backend/src/main/java/program/controllers/MovieController.java
@@ -4,17 +4,22 @@ package program.controllers;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import program.entities.Movie;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import program.repositories.MovieRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class MovieController {
+
+    @Autowired
+    MovieRepository movieRepository;
 
     @RequestMapping("/search")
     public static List<Movie> searchByTitle(@RequestParam(value="title") String title){
@@ -40,4 +45,25 @@ public class MovieController {
         return searchResult;
     }
 
+    @RequestMapping("/movie")
+    public Movie searchById(@RequestParam(value = "id") String id){
+        Movie movie = new Movie();
+        RestTemplate template = new RestTemplate();
+        String result = template.getForObject("http://www.omdbapi.com/?i=" + id + "&apikey=fda66a87", String.class);
+
+        JsonObject jsonObject = new JsonParser().parse(result).getAsJsonObject();
+        movie.setIMDBId(jsonObject.get("imdbID").getAsString());
+        movie.setTitle(jsonObject.get("Title").getAsString());
+        movie.setYear(jsonObject.get("Year").getAsString());
+        movie.setGenre(jsonObject.get("Genre").getAsString());
+        movie.setLanguage(jsonObject.get("Language").getAsString());
+        movie.setRated(jsonObject.get("Rated").getAsString());
+        movie.setRuntime(jsonObject.get("Runtime").getAsString());
+        movie.setPlot(jsonObject.get("Plot").getAsString());
+        movie.setPoster(jsonObject.get("Poster").getAsString());
+
+        movieRepository.save(movie);
+
+        return movie;
+    }
 }
